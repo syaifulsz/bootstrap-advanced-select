7b62d6f89bc9b3a2db3f600e01c9edaf967c95c1
syaifulsz <hello@syaifulshahzinan.com>
2013-10-06 01:00:10 +0800

    Added click tracking

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+    $('#btnDownloadTop').on('click', function() {
+        _gaq.push(['_trackEvent', 'Bootstrap Advanced Select', 'Download', 'Button Download - Top']);
+    });
+
+    $('#btnDownloadBottom').on('click', function() {
+        _gaq.push(['_trackEvent', 'Bootstrap Advanced Select', 'Download', 'Button Download - Bottom']);
+    });
+
     // Chosen
     //==================================================================================================================
 
